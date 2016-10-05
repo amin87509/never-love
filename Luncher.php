d7c9f706121fe05151704f40519c08e5f8bc7760
amin87509 <amin87509@gmail.com>
2016-10-05 19:47:04 -0400

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '**236192756:AAEvOMCozBfeqXSCLNEXfQAFU7uKDHG4nv4**');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
